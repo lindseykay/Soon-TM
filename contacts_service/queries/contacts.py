36fe8df0c39255312aba8cdb9123bb1f61d778db
Ron <rsamemiya@gmail.com>
2022-11-18 17:36:34 -0800

    completed get_all_contacts endpoint.  it is now functional

--- contacts_service/queries/contacts.py
+++ contacts_service/queries/contacts.py
@@ -19,6 +19,7 @@ class ContactIn(BaseModel):
     notes : str
 
 class ContactOut(BaseModel):
+    id : int
     user_id: int
     recipient : Recipient
     special_days : List[SpecialDayOut]
@@ -40,7 +41,7 @@ class ContactsRepository:
                         [contact.user_id, contact.recipient_id, contact.notes]
                     )
                     contact_record = result.fetchone()
-                    recipient = self.temp_create_recipient(contact_record[1])
+                    recipient = temp_create_recipient(contact_record[1])
 
                     s_days = []
                     for day in special_days:
@@ -48,6 +49,7 @@ class ContactsRepository:
                         if isinstance(new_sd,SpecialDayOut):
                             s_days.append(new_sd)
                     return ContactOut(
+                        id = contact_record[0],
                         user_id = contact_record[1],
                         recipient = recipient,
                         special_days = s_days,
@@ -56,11 +58,77 @@ class ContactsRepository:
         except Exception:
             return {"message": "hello error town"}
 
+    def get_all(self, user_id: int)-> Union[List[ContactOut],ContactError]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        SELECT *
+                        FROM CONTACTS
+                        WHERE user_id = %s
+                        """,
+                        [user_id]
+                    )
+                    query = result.fetchall()
 
-    def temp_create_recipient(self, id:int):
-        return Recipient(
-            id = id,
-            name = "Mom",
-            phone = None,
-            email = None
-        )
+                    return [query_to_contactout(record) for record in query]
+
+
+        except Exception:
+            return {"message": "hello Can't find all contacts"}
+
+    #  [(contact id =1,
+    #   user_id= 1,
+    #   recipient_id =1000,
+    #   note = 'Hi')
+
+    # class ContactOut(BaseModel):
+    # user_id: int V
+    # recipient : Recipient use temp_create_recipient function
+    # special_days : List[SpecialDayOut]
+    # notes : str
+
+def temp_create_recipient(id:int):
+    return Recipient(
+        id = id,
+        name = "Mom",
+        phone = None,
+        email = None
+    )
+
+
+def query_to_contactout(query:tuple) -> ContactOut:
+    with pool.connection() as conn:
+        with conn.cursor() as db:
+            result = db.execute(
+                """
+                SELECT *
+                FROM special_days
+                WHERE contact_id = %s
+                """,
+                [query[0]]
+            )
+            sd_query = result.fetchall()
+            specialdays = [query_to_specialdayout(record) for record in sd_query]
+            recipient = temp_create_recipient(query[2])
+            return ContactOut(
+                id = query[0],
+                user_id = query[1],
+                recipient = recipient,
+                special_days = specialdays,
+                notes = query[3]
+            )
+
+def query_to_specialdayout(query:tuple) -> SpecialDayOut:
+    return SpecialDayOut(
+        id = query[0],
+        contact_id = query[1],
+        name = query[2],
+        date = query[3]
+    )
+
+
+
+
+
--- contacts_service/routers/contacts.py
+++ contacts_service/routers/contacts.py
@@ -17,3 +17,13 @@ def create_contact(contact: ContactIn,
     special_days: List[SpecialDayIn] = []) -> ContactOut:
     new_contact = repo.create(contact, special_days)
     return new_contact
+
+
+@router.get("/contacts", response_model = Union[List[ContactOut],ContactError])
+def get_all_contacts(user_id: int,
+    response: Response,
+    repo: ContactsRepository=Depends()) -> List[ContactOut]:
+    contacts_list = repo.get_all(user_id)
+    return contacts_list
+
+
