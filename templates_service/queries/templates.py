e4c566949badee58fd5eeab1c81ef9223b9cae21
Sean Li <sean.h.li.2017@gmail.com>
2022-11-19 17:50:03 -0800

    completed all endpoints for templates, will need to add endpoints for themes

--- templates_service/queries/templates.py
+++ templates_service/queries/templates.py
@@ -13,9 +13,12 @@ class PublicTemplateIn(BaseModel):
     content: str
 
 class TemplateIn(BaseModel):
-    user_id: int
     name: str
     content: str
+
+class TemplateUpdate(BaseModel):
+    name: Optional[str]
+    content: Optional[str]
 
 class TemplateOut(BaseModel):
     id: int
@@ -24,6 +27,10 @@ class TemplateOut(BaseModel):
     user_id: Optional[int]
     name: str
     content: str
+
+class TemplatesOut(BaseModel):
+    public_templates: List[TemplateOut]
+    user_templates: List[TemplateOut]
 
 class TemplateError(BaseModel):
     message: str
@@ -66,11 +73,158 @@ class TemplateRepository:
         except Exception:
             return {"message": "My disappointment is immeasurable and my day is ruined"}
 
-    def create_user_template(self, template: TemplateIn) -> Union[TemplateOut,TemplateError]:
-        pass
+    def create_user_template(self, template: TemplateIn, user_id: int) -> Union[TemplateOut,TemplateError]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        INSERT INTO templates
+                            (public, user_id, name, content)
+                        VALUES
+                            (%s, %s, %s, %s)
+                        RETURNING *;
+                        """,
+                        [
+                            False,
+                            user_id,
+                            template.name,
+                            template.content
+                        ]
+                    )
+                    record = result.fetchone()
+                    print(record)
 
-    def get_all(self, user_id: int) -> Union[List[TemplateOut],TemplateError]:
-        pass
+                    output = TemplateOut(
+                        id = record[0],
+                        public = record[1],
+                        theme_id = record[2],
+                        user_id = record[3],
+                        name = record[4],
+                        content = record[5]
+                    )
+                    return output
+
+        except Exception:
+            return {"message": "My disappointment is immeasurable and my day is ruined"}
+
+    def get_all(self, user_id: Optional[int]) -> Union[TemplatesOut,TemplateError]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        SELECT *
+                        FROM templates
+                        WHERE user_id = %s OR user_id IS NULL
+                        ORDER BY public DESC
+                        """,
+                        [
+                            user_id
+                        ]
+                    )
+                    query = result.fetchall()
+
+                    public_templates = [TemplateOut(
+                        id = record[0],
+                        public = record[1],
+                        theme_id = record[2],
+                        user_id = record[3],
+                        name = record[4],
+                        content = record[5]) for record in query if record[1]]
+                    user_templates = [] if not user_id else [TemplateOut(
+                        id = record[0],
+                        public = record[1],
+                        theme_id = record[2],
+                        user_id = record[3],
+                        name = record[4],
+                        content = record[5]) for record in query if not record[1]]
+
+                    return TemplatesOut(
+                        public_templates = public_templates,
+                        user_templates = user_templates
+                    )
+
+        except Exception:
+            return {"message": "My disappointment is immeasurable and my day is ruined"}
 
     def get_template(self, template_id: int, user_id: int) -> Union[TemplateOut,TemplateError]:
-        pass
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        SELECT *
+                        FROM templates
+                        WHERE user_id = %s AND id = %s
+                        """,
+                        [
+                            user_id,
+                            template_id
+                        ]
+                    )
+                    record = result.fetchone()
+
+                    return TemplateOut(
+                        id = record[0],
+                        public = record[1],
+                        theme_id = record[2],
+                        user_id = record[3],
+                        name = record[4],
+                        content = record[5]
+                    )
+        except Exception:
+            return {"message": "My disappointment is immeasurable and my day is ruined"}
+
+    def update_template(self, template_id: int, user_id: int, info: TemplateUpdate) -> Union[TemplateOut, TemplateError]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        UPDATE templates
+                        SET name = COALESCE(%s, name)
+                            , content = COALESCE(%s, content)
+                        WHERE user_id = %s AND id = %s
+                        RETURNING *;
+                        """,
+                        [
+                            info.name,
+                            info.content,
+                            user_id,
+                            template_id
+                        ]
+                    )
+                    record = result.fetchone()
+
+                    return TemplateOut(
+                        id = record[0],
+                        public = record[1],
+                        theme_id = record[2],
+                        user_id = record[3],
+                        name = record[4],
+                        content = record[5]
+                    )
+        except Exception:
+            return {"message": "My disappointment is immeasurable and my day is ruined"}
+
+    def delete_template(self, template_id: int, user_id: int) -> bool:
+        try:
+            if not user_id:
+                return False
+
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        DELETE FROM templates
+                        where user_id = %s AND id = %s
+                        """,
+                        [
+                            user_id,
+                            template_id
+                        ]
+                    )
+                    return True
+        except Exception:
+            return False
--- templates_service/routers/templates.py
+++ templates_service/routers/templates.py
@@ -3,7 +3,9 @@ from typing import List, Optional, Union
 from queries.templates import (
     PublicTemplateIn,
     TemplateIn,
+    TemplateUpdate,
     TemplateOut,
+    TemplatesOut,
     TemplateError,
     TemplateRepository,
     ThemeOut
@@ -29,13 +31,49 @@ def create_template(
     response: Response,
     repo: TemplateRepository = Depends()
     ) -> TemplateOut:
-    pass
+    new_template = repo.create_user_template(template, user_id)
+    return new_template
 
 
-@router.get("/templates", response_model = Union[List[TemplateOut],TemplateError])
+@router.get("/templates", response_model = Union[TemplatesOut,TemplateError])
 def get_all_templates(
     response: Response,
     repo: TemplateRepository = Depends(),
     user_id: int = None
-    ) -> List[TemplateOut]:
-    pass
+    ) -> TemplatesOut:
+    all_templates = repo.get_all(user_id)
+    return all_templates
+
+
+@router.get("/templates/{template_id}", response_model = Union[TemplateOut,TemplateError])
+def get_template(
+    template_id: int,
+    user_id: int,
+    response: Response,
+    repo: TemplateRepository = Depends(),
+    ) -> TemplatesOut:
+    template = repo.get_template(template_id, user_id)
+    return template
+
+
+@router.put("/templates/{template_id}", response_model = Union[TemplateOut,TemplateError])
+def update_template(
+    template_id: int,
+    user_id: int,
+    info: TemplateUpdate,
+    response: Response,
+    repo: TemplateRepository = Depends()
+    ) -> TemplateOut:
+    updated_template = repo.update_template(template_id, user_id, info)
+    return updated_template
+
+
+@router.delete("/templates/{template_id}", response_model = bool)
+def delete_template(
+    template_id: int,
+    user_id: int,
+    response: Response,
+    repo: TemplateRepository = Depends()
+    ) -> bool:
+    deleted_flag = repo.delete_template(template_id, user_id)
+    return deleted_flag
