a7f8f2406b62857bc7d8e552d0a0c1705fc97cb2
Ron <rsamemiya@gmail.com>
2022-12-02 17:38:49 -0800

    made nav branch, burger side bar and css for burger side bar

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -1,22 +1,38 @@
 import { useToken } from "./hooks/useToken";
 import LoginWidget from "./loginWidget";
 
-function NavBar() {
-    const [token,,logout] = useToken()
+import React from "react";
+import { stack as Menu } from "react-burger-menu";
+import "./Sidebar.css"; // Decide with team if we want to implement this into index.css to clean up directory
 
-    return (
-        <div className="nav-bar">
-            {!token &&
-                <LoginWidget />
-            }
-            {token &&
-                <>
-                    <div>menu placeholder</div>
-                    <button onClick={e=>logout()}>Log out</button>
-                </>
-            }
-        </div>
-    )
+function NavBar() {
+  const [token, , logout] = useToken();
+
+  return (
+    <div className="nav-bar">
+      {!token && <LoginWidget />}
+      {token && (
+        <>
+          <div>menu placeholder</div>
+          <button onClick={(e) => logout()}>Log out</button>
+        </>
+      )}
+      <Menu>
+        <a className="menu-item" href="/">
+          Home
+        </a>
+        <a className="menu-item" href="/">
+          My Reminders
+        </a>
+        <a className="menu-item" href="/">
+          My Contacts
+        </a>
+        <a className="menu-item" href="/">
+          Settings
+        </a>
+      </Menu>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
