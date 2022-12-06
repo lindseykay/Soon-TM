008212448831a8aed454e5e1e5518b5ff763e4a5
Alex <pallotaw@gmail.com>
2022-12-06 14:55:49 -0800

    Changes to contacts Dockerfile and docker-compose.yaml for deployment

--- contacts_service/queries/pools.py
+++ contacts_service/queries/pools.py
@@ -3,4 +3,4 @@ from psycopg_pool import ConnectionPool
 
 pool = ConnectionPool(conninfo=os.environ["DATABASE_URL"])
 
-reminder_pool = ConnectionPool(conninfo="postgresql://postgres:password@db:5432/reminders")
+reminder_pool = ConnectionPool(conninfo=os.environ["DATABASE_URL2"])
