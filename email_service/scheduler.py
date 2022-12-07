d08cd542fe5af896aa662910c11d268303ac91f2
Alex <pallotaw@gmail.com>
2022-12-06 19:44:20 -0800

    removed everythin

--- email_service/scheduler.py
+++ email_service/scheduler.py
@@ -1,30 +1,31 @@
-import schedule
-from datetime import date
-from emailer import formatter, send_emails
-import requests
-import json
-import os
+# import schedule
+# from datetime import date
+# from emailer import formatter, send_emails
+# import requests
+# import json
+# import os
 
-def reminder_compiler():
-    url = f'{os.environ["REMINDERS_HOST"]}{os.environ["COMPILER_ROUTE"]}'
-    response = requests.get(url)
-    content = json.loads(response.content)
-    return content
+# def reminder_compiler():
+#     url = f'{os.environ["REMINDERS_HOST"]}{os.environ["COMPILER_ROUTE"]}'
+#     response = requests.get(url)
+#     content = json.loads(response.content)
+#     return content
 
-def job():
-    x = reminder_compiler()
-    try:
-        y = formatter(x)
-        send_emails(y)
-    except Exception:
-        print("You have no reminders or crashed")
+# def job():
+#     x = reminder_compiler()
+#     try:
+#         y = formatter(x)
+#         send_emails(y)
+#     except Exception:
+#         print("You have no reminders or crashed")
 
-#   ----SCHEDULER----
+# #   ----SCHEDULER----
 
 def compiler_scheduler():
-    schedule.every().day.at("14:00:00").do(job)
-    schedule.every().day.at("15:00:00").do(job)
-    schedule.every().day.at("19:00:00").do(job)
+    pass
+#     schedule.every().day.at("14:00:00").do(job)
+#     schedule.every().day.at("15:00:00").do(job)
+#     schedule.every().day.at("19:00:00").do(job)
 
-    while True:
-        schedule.run_pending()
+#     while True:
+#         schedule.run_pending()
