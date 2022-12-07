ea08f05c9f0d056c12ffefae2ef3cb40e05e0edb
Alex <pallotaw@gmail.com>
2022-12-06 19:56:20 -0800

    changes to dockerfile, and added print statements to scheduler.py and main.py

--- email_service/main.py
+++ email_service/main.py
@@ -1,3 +1,4 @@
 from scheduler import compiler_scheduler
 
+print("HELLO FROM main.py")
 compiler_scheduler()
--- email_service/scheduler.py
+++ email_service/scheduler.py
@@ -1,31 +1,32 @@
-# import schedule
-# from datetime import date
-# from emailer import formatter, send_emails
-# import requests
-# import json
-# import os
+import schedule
+from datetime import date
+from emailer import formatter, send_emails
+import requests
+import json
+import os
 
-# def reminder_compiler():
-#     url = f'{os.environ["REMINDERS_HOST"]}{os.environ["COMPILER_ROUTE"]}'
-#     response = requests.get(url)
-#     content = json.loads(response.content)
-#     return content
+def reminder_compiler():
+    url = f'{os.environ["REMINDERS_HOST"]}{os.environ["COMPILER_ROUTE"]}'
+    response = requests.get(url)
+    content = json.loads(response.content)
+    return content
 
-# def job():
-#     x = reminder_compiler()
-#     try:
-#         y = formatter(x)
-#         send_emails(y)
-#     except Exception:
-#         print("You have no reminders or crashed")
+def job():
+    x = reminder_compiler()
+    try:
+        y = formatter(x)
+        send_emails(y)
+    except Exception:
+        print("You have no reminders or crashed")
 
-# #   ----SCHEDULER----
+#   ----SCHEDULER----
 
 def compiler_scheduler():
-    pass
-#     schedule.every().day.at("14:00:00").do(job)
-#     schedule.every().day.at("15:00:00").do(job)
-#     schedule.every().day.at("19:00:00").do(job)
+    print("FIRST PRINT")
+    schedule.every().day.at("14:00:00").do(job)
+    schedule.every().day.at("15:00:00").do(job)
+    schedule.every().day.at("19:00:00").do(job)
 
-#     while True:
-#         schedule.run_pending()
+    while True:
+        print("WOW")
+        schedule.run_pending()
