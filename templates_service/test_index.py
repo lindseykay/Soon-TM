32331eba98c6835e01beb695b34f4a1580f8498e
Sean Li <sean.h.li.2017@gmail.com>
2022-12-08 17:00:52 -0800

    completed create public templates unit test

--- templates_service/test_index.py
+++ templates_service/test_index.py
@@ -30,3 +30,46 @@ def test_get_all_templates():
     assert response.json() == expected
 
     app.dependency_overrides = {}
+
+
+class CreatePublicTempRepo:
+    def create_public_templates(self, templates):
+        result = [{
+            "id": 1,
+            "public": True,
+            "theme_id": template.theme_id,
+            "user_id": None,
+            "name": template.name,
+            "content": template.content
+        } for template in templates]
+        return result
+
+def test_create_public_templates():
+
+    app.dependency_overrides[TemplateRepository] = CreatePublicTempRepo
+
+    input = [
+        {
+            "theme_id": 0,
+            "name": "string",
+            "content": "string"
+        }
+        ]
+
+    expected = [
+        {
+            "id": 1,
+            "public": True,
+            "theme_id": 0,
+            "user_id": None,
+            "name": "string",
+            "content": "string"
+        }
+        ]
+
+    response = client.post("/public/templates/", json=input)
+
+    assert response.status_code == 200
+    assert response.json() == expected
+
+    app.dependency_overrides = {}
